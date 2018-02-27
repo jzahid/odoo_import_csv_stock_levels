cec1eabbdbaba32bf740042fa79b3b7812913034
zahid <j.zahid@gmail.com>
2018-02-27 19:53:47 +0000

    Update import_inventory.py

--- wizard/import_inventory.py
+++ wizard/import_inventory.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright 2018 Aurium Technologies (<http://www.auriumtechnologies.com>)
 # Copyright 2016 Avanzosc (<http://www.avanzosc.es>)
 # Copyright 2016 Tecnativa (<http://www.tecnativa.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
